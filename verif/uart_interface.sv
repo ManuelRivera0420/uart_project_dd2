401eecfb2afa6b9c47aede4b724b80f0a5cca606
ManuelRivera0420 <manuel2004rj@gmail.com>
2026-01-22 10:47:35 -0600

    Updated interface and tb file

--- verif/uart_interface.sv
+++ verif/uart_interface.sv
@@ -10,28 +10,29 @@
 
 interface uart_interface(input logic clk, input logic arst_n);
 
-// CSR parameters
+// UART PARAMETERS
 parameter BYTE_WIDTH = 8;
 parameter BIT_SAMPLING = 15;
 parameter HALFBIT_SAMPLING = 7;
 
-// Declaration of signals used by user tile
+// Declaration of signals used by the uart top
 logic [BYTE_WIDTH - 1 : 0] data_out;
 logic [BYTE_WIDTH - 1 : 0] data_in;
 logic tx_start;
 logic tx_done;
 logic rx_done;
 
-// This modport should be used by user to connect with his/her tile logic
+// modport
 modport user_tile_modport(
 	input data_in, tx_start,
 	output data_out, tx_done, rx_done
 );
 
-// This task can be used to assign a value to data_reg_a user tile input
+// This task can be used to transmit data from the transmitter into the
+// receiver module of the uart
 task write_data_in(logic [BYTE_WIDTH - 1 : 0] data);
-	@(posedge clk);
-	data_in <= data;
+    @(posedge clk);
+    data_in <= data;
     @(posedge clk);
     tx_start <= 1'b1;
     @(posedge clk);
--- verif/uart_top_tb.sv
+++ verif/uart_top_tb.sv
@@ -4,28 +4,19 @@ localparam BYTE_WIDTH = 8;
 
 bit clk;
 bit arst_n;
-logic [BYTE_WIDTH - 1 : 0] data_in;
-logic tx_start;
-logic tx_done;
-logic rx_done;
-logic [BYTE_WIDTH - 1 : 0] data_out;
+
+uart_interface intf(clk, arst_n);
 
 always #5ns clk = ~clk;
 assign #50ns arst_n = 1'b1;
 
+logic [BYTE_WIDTH - 1 : 0] rand_data;
 initial begin
-    data_in = '0;
-    tx_start = 1'b0;
-    repeat (10) begin
-        std::randomize(data_in);
-        #1ms;
-        @(posedge clk);
-        tx_start = 1'b1;
-        @(posedge clk);
-        tx_start = 1'b0;
-        @(posedge clk);
-        #1ms;
-    end    
+	repeat(100) begin
+		std::randomize(rand_data);
+		intf.write_data_in(rand_data);
+		#1200us;
+	end
 end
 
 initial begin
@@ -33,42 +24,54 @@ initial begin
     $finish;
 end
 
+localparam BAUD_CNT_CYCLES = ((100_000_000) / (9600 * 16));
 logic [7:0] data_in_temp;
+logic [31:0] baud_counter_tb;
+
+always_ff @(posedge clk or negedge arst_n) begin
+	if(!arst_n) begin
+		baud_counter_tb <= '0;
+	end else if (baud_counter_tb == BAUD_CNT_CYCLES) begin
+		baud_counter_tb <= baud_counter_tb <= '0;
+	end else begin
+		baud_counter_tb <= baud_counter_tb + 1'b1;
+	end
+end
 
 always_ff @(posedge clk or negedge arst_n) begin
   if (!arst_n)
     data_in_temp <= '0;
-  else if (tx_start)
-    data_in_temp <= data_in;
+  else if (intf.tx_start)
+    data_in_temp <= intf.data_in;
 end
 
 
 uart_top uart_top_i(
 .clk(clk),
 .arst_n(arst_n),
-.data_in(data_in),
-.tx_start(tx_start),
-.tx_done(tx_done),
-.rx_done(rx_done),
-.data_out(data_out)
+.data_in(intf.data_in),
+.tx_start(intf.tx_start),
+.tx_done(intf.tx_done),
+.rx_done(intf.rx_done),
+.data_out(intf.data_out)
 );
 
 assert property(
     @(posedge clk)
     disable iff(!arst_n)
-    rx_done |-> (data_out == data_in_temp)
+   intf.rx_done |-> (intf.data_out == data_in_temp)
 );
 
 assert property (
   @(posedge clk) 
   disable iff(!arst_n)
-  $rose(tx_start) |-> ##[100000:115000] ($rose(tx_done) && (data_out == data_in_temp))
+  $rose(intf.tx_start) |-> ##[100000:115000] ($rose(intf.tx_done) && (intf.data_out == data_in_temp))
 );
 
 assert property (
   @(posedge clk) 
   disable iff(!arst_n)
-  $rose(tx_start) |=>  (~uart_top_i.transmitter_i.tx)
+  $rose(intf.tx_start) |=>  (~uart_top_i.transmitter_i.tx)
 );
 
 initial begin
