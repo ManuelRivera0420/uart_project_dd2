c856bf66d4e0d956cb678c889d6d3fa6db4e6602
ManuelRivera0420 <manuel2004rj@gmail.com>
2026-01-22 01:05:30 -0600

    cleaned workspace

--- verif/work/xcelium.d/run.lnx8664.25.03.d/files.ts
+++
@@ -1,5 +0,0 @@
-1768849854 /home/dani/manuel/uart_project_dd2/rtl/baud_rate_generator.sv
-1768849854 /home/dani/manuel/uart_project_dd2/rtl/receiver.sv
-1768849854 /home/dani/manuel/uart_project_dd2/rtl/transmitter.sv
-1768849854 /home/dani/manuel/uart_project_dd2/rtl/uart_top.sv
-1768850105 /home/dani/manuel/uart_project_dd2/verif/uart_top_tb.sv
